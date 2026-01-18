741f22a101606c758ebd889edef169006e7b04fb
Pawan <pawanmkolachippu@gmail.com>
2026-01-18 14:44:32 +0530

    Configure backend to connect to Postgres

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,34 @@
 const express = require('express');
+const { Pool } = require('pg');
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {
-  res.json({ message: "Hello from the DevOps GitOps Pipeline!", timestamp: new Date() });
+// Connect to Postgres using Env Vars (which we will set in K8s next)
+const pool = new Pool({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    port: 5432,
+});
+
+app.get('/', async (req, res) => {
+    try {
+        // Try to query the database
+        const result = await pool.query('SELECT NOW()');
+        res.json({
+            message: "✅ Backend successfully connected to Postgres Database!",
+            db_time: result.rows[0].now
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            message: "❌ Database Connection Failed",
+            error: err.message
+        });
+    }
 });
 
 app.listen(port, () => {
-  console.log(`Backend API listening at http://localhost:${port}`);
+    console.log(`API listening on port ${port}`);
 });
